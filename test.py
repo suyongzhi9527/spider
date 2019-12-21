c837bbdd2a79045ff897b81f05e5fc2d0849164c
suyongzhi <1125699801@qq.com>
2019-12-21 10:28:51 +0800

    测试

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
 from lxml import etree
 import requests
 
-url = 'https://tieba.baidu.com/mo/q---771EA4758368849F1B99568B4C102059:FG=1-sz@320_240,,-2-3-0--2/m?kw=python&pn=0&'
-res = requests.get(url)
-html_str = res.content.decode()
-
-html = etree.HTML(html_str)
-
-li_list = html.xpath("//li[contains(@class,'tl_shadow tl_shadow_new')]")
-for li in li_list:
-    item = {}
-    item['title'] = li.xpath(".//div[@class='ti_title']/span/text()")
-    print(item)
