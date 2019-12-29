95f8575107aba481d94c6b5303f65b2b7c8dd86d
suyongzhi <1125699801@qq.com>
2019-12-29 17:48:58 +0800

    人人网cookies登录

--- cookies_login.py
+++ cookies_login.py
@@ -1,10 +1,31 @@
-import json
-import requests
+from urllib import request,parse
+from http.cookiejar import CookieJar
 
-url = "https://m.douban.com/rexxar/api/v2/movie/suggestion?start=0&count=10&new_struct=1&with_review=1&for_mobile=1"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+}
 
-res = requests.get(url)
+def get_opener():
+    cookiejar = CookieJar()
+    handler = request.HTTPCookieProcessor(cookiejar)
+    opener = request.build_opener(handler)
+    return opener
 
-print(res.status_code)
-dict_ret = json.loads(res.content.decode())
-print(dict_ret)
+def login_renren(opener):
+    data = {"email": "970138074@qq.com", "password": "pythonspider"}
+    data = parse.urlencode(data).encode('utf-8')
+    login_url = "http://www.renren.com/PLogin.do"
+    req = request.Request(login_url, headers=headers, data=data)
+    opener.open(req)
+
+def visit_profile(opener):
+    url = 'http://www.renren.com/880151247/profile'
+    req = request.Request(url,headers=headers)
+    resp = opener.open(req)
+    with open('renren.html','w') as fp:
+        fp.write(resp.read().decode("utf-8"))
+
+if __name__ == '__main__':
+    opener = get_opener()
+    login_renren(opener)
+    visit_profile(opener)
