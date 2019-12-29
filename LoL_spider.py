af742e475ff18b95a83052c99afef1876d5c7d40
suyongzhi <1125699801@qq.com>
2019-12-29 12:51:44 +0800

    爬取LOL英雄皮肤

--- LoL_spider.py
+++ LoL_spider.py
@@ -9,6 +9,8 @@ import re
 3.获取下载图片名称
 4.保存图片
 """
+
+
 def get_lol_image():
     # 获取JS源代码
     url = 'https://lol.qq.com/biz/hero/champion.js'
@@ -16,7 +18,7 @@ def get_lol_image():
 
     # 正则表达式
     regx = r'"keys":(.*?),"data"'
-    js_list = re.findall(regx,html_str) # 列表类型
+    js_list = re.findall(regx, html_str)  # 列表类型
     # print(type(js_list)) # 列表
     # print(type(js_list[0])) # 字符串
     js_dict = json.loads(js_list[0])
@@ -28,13 +30,13 @@ def get_lol_image():
             i = str(i)
             # https://game.gtimg.cn/images/lol/act/img/skin/big154015.jpg
             if len(i) == 1:
-                hero_num = "00" + i # 001 002 003
+                hero_num = "00" + i  # 001 002 003
             elif len(i) == 2:
-                hero_num = "0" + i # 010 020 030
+                hero_num = "0" + i  # 010 020 030
             hero_nums = hero_id + hero_num
             hero_url = 'https://game.gtimg.cn/images/lol/act/img/skin/big'+hero_nums+'.jpg'
             pic_list.append(hero_url)
-    
+
     # 获取下载图片名称
     list_filepath = []
     path = "F:\spider_learn\images\\"
@@ -50,6 +52,8 @@ def get_lol_image():
         n += 1
         if res.status_code == 200:
             print("正在下载:%s" % list_filepath[n])
-            with open(list_filepath[n],'wb') as f:
+            with open(list_filepath[n], 'wb') as f:
                 f.write(res.content)
+
+
 get_lol_image()
