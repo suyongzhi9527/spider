bcb3a5d507d7854289e56a1fe695c6675a248a2d
suyongzhi <1125699801@qq.com>
2020-01-08 21:59:47 +0800

    保存结果为csv格式文件

--- 36kr.py
+++ 36kr.py
@@ -1,5 +1,6 @@
 import re
 import json
+import csv
 import requests
 
 url = 'https://36kr.com/'
@@ -7,10 +8,25 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
 }
 
-res = requests.get(url,headers=headers)
+res = requests.get(url, headers=headers)
 
 html_str = res.content.decode()
 
-ret = re.findall("<script>window.initialState=(.*?)</script>",html_str)[0]
+ret = re.findall("<script>window.initialState=(.*?)</script>", html_str)[0]
 ret = json.loads(ret)
-print(ret["homeData"]["data"]["bannerLeft"]["data"][0])
+item_list = []
+for i in range(len(ret["homeData"]["data"]["latestArticle"]["data"])):
+    item = {}
+    item['title'] = ret["homeData"]["data"]["latestArticle"]["data"][i]["post"]["title"]
+    item['cover'] = ret["homeData"]["data"]["latestArticle"]["data"][i]["post"]["cover"]
+    item['summary'] = ret["homeData"]["data"]["latestArticle"]["data"][i]["post"]["summary"]
+    item['published_at'] = ret["homeData"]["data"]["latestArticle"]["data"][i]["post"]["published_at"]
+    item_list.append(item)
+
+    with open("36kr.csv","w",encoding="utf-8-sig",newline="") as f:
+        headers = ['title','cover','summary','published_at']
+        writer = csv.DictWriter(f,headers)
+        writer.writeheader()
+        writer.writerows(item_list)
+        # for item_li in item_list:
+        #     f.write(json.dumps(item_li,ensure_ascii=False,indent=2))
