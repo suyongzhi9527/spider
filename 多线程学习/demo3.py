449901f3a737eaa2b8f8435020eaca4fe36575c1
suyongzhi <1125699801@qq.com>
2020-01-10 11:09:50 +0800

    多线程学习

--- 多线程学习/demo3.py
+++ 多线程学习/demo3.py
@@ -1,60 +1,21 @@
 import threading
-import random
-import time
 
-gMoney = 1000
-gTotalTimes = 10
-gLock = threading.Lock()
-gTimes = 0
+VALUE = 0
 
+Lock = threading.Lock() # 创建一个锁对象
 
-class Producer(threading.Thread):
-    def run(self):
-        global gMoney
-        global gTimes
-        while True:
-            money = random.randint(100, 1000)
-            gLock.acquire()
-            if gTimes >= 10:
-                gLock.release()
-                break
-            gMoney += money
-            print("%s生产了%d元钱,剩余%d元钱" %
-                  (threading.current_thread(), money, gMoney))
-            gTimes += 1
-            gLock.release()
-            time.sleep(0.5)
-
-
-class Consumer(threading.Thread):
-    def run(self):
-        global gMoney
-        while True:
-            money = random.randint(100, 1000)
-            gLock.acquire()
-            if gMoney >= money:
-                gMoney -= money
-                print("%s消费了%d元钱，剩余%d元钱" %
-                      (threading.current_thread(), money, gMoney))
-            else:
-                if gTimes >= gTotalTimes:
-                    gLock.release()
-                    break
-                print("%s消费者消费了%d元钱，剩余%d元钱,不足!" %
-                      (threading.current_thread(), money, gMoney))
-            gLock.release()
-            time.sleep(0.5)
-
+def add_value():
+    global VALUE
+    Lock.acquire() # 上锁
+    for i in range(1000000):
+        VALUE += 1
+    Lock.release() # 解锁
+    print(VALUE)
 
 def main():
-    for i in range(3):
-        t = Consumer(name="消费者线程%s" % i)
+    for i in range(2):
+        t = threading.Thread(target = add_value)
         t.start()
-
-    for i in range(5):
-        t = Producer(name="生产者线程%s" % i)
-        t.start()
-
 
 if __name__ == "__main__":
     main()
