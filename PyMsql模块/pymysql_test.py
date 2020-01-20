c6b5b863d5c530f460d0dbf7ce390788258a756c
suyongzhi <1125699801@qq.com>
2020-01-20 22:14:42 +0800

    Python连接Mysql,插入抓取下来的数据保存到数据库

--- PyMsql模块/pymysql_test.py
+++ PyMsql模块/pymysql_test.py
@@ -1,18 +1,35 @@
 import pymysql
+import requests
+from bs4 import BeautifulSoup
 
 db = pymysql.connect(
     host='localhost',
     user='root',
     password='123456',
-    database='school',
+    database='scraping',
     port=3306
 )
 
 cursor = db.cursor()  # 创建一个游标对象
 # 查询数据
-# cursor.execute("select * from tb_college")
+# cursor.execute("select * from urls")
 # result = cursor.fetchall()
 # print(result)
+
+link = 'http://www.santostang.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+}
+r = requests.get(link, headers=headers)
+soup = BeautifulSoup(r.text, 'lxml')
+title_list = soup.find_all("h1", class_="post-title")
+for eachone in title_list:
+    url = eachone.a['href']
+    title = eachone.a.text.strip()
+    cursor.execute("insert into urls(url,content) values (%s, %s)", (url, title))
+cursor.close()
+db.commit()
+db.close()
 
 # 条件查询
 # sql = """
@@ -82,4 +99,4 @@ cursor = db.cursor()  # 创建一个游标对象
 # cursor.execute(sql)
 # db.commit()
 
-db.close()
+# db.close()
