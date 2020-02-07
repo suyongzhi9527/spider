c2f8fb14931fd04b8d9ff0035d088983db016896
suyongzhi <1125699801@qq.com>
2020-02-07 22:46:54 +0800

    前程无忧招聘信息抓取

--- 51job/51job_spider.py
+++ 51job/51job_spider.py
@@ -7,35 +7,70 @@
 """
 import requests
 import re
+import csv
 from lxml import etree
+from urllib import parse
 
-url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,Python%25E7%2588%25AC%25E8%2599%25AB,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+key = '实施工程师'
+key = parse.quote(key)
+
+# 伪装爬虫头部，防止被网站禁止
+headers = {'Host': 'search.51job.com',
+           'Upgrade-Insecure-Requests': '1',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)\
+         Chrome/63.0.3239.132 Safari/537.36'}
+
+data = open('Job_data.csv', 'w', newline='')
+writer = csv.writer(data)
+writer.writerow(
+    ('link', 'job', 'salary', 'company', 'area', 'experience', 'education', 'companytype', 'direction', 'describe'))
+
+
+def get_links(page):
+    url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,' + key + ',2,' + str(page) + '.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
+    response = requests.get(url, headers=headers, timeout=10)
+    s = requests.session()
+    s.keep_alive = False
+    response.encoding = 'gbk'
+    html = etree.HTML(response.text)
+    # reg = re.compile(r'class="t1 ">.*? <a target="_blank" title=".*?" href="(.*?)".*? <span class="t2">', re.S)
+    # url_list = re.findall(reg, response.text)
+    url_list = html.xpath('//div[@id="resultList"]//div[@class="el"]/p/span/a/@href')
+    return url_list
 
 
 def content_data(url):
-    item = {}
-    detail_url = requests.get(url)
+    detail_url = requests.get(url, headers=headers, timeout=10)
+    s = requests.session()
+    s.keep_alive = False
     detail_url.encoding = 'gbk'
     detail_html = etree.HTML(detail_url.text)
-    title = detail_html.xpath('//div[@class="cn"]/h1/@title')[0]
-    salary = detail_html.xpath('//div[@class="cn"]//strong/text()')[0]
-    company = detail_html.xpath('//div[@class="cn"]//p[@class="cname"]/a/@title')[0]
-    msg_ltype = detail_html.xpath('//div[@class="cn"]//p[@class="msg ltype"]/@title')[0]
-    msg_ltype = re.sub(r'[\s\|]', '', msg_ltype)
-    tbordertop_box = "".join(detail_html.xpath('//div[@class="bmsg job_msg inbox"]//p/text()')).strip()
-    item = {
-        'title': title,
-        'salary': salary,
-        'company': company,
-        'msg': msg_ltype,
-        'tbordertop_box': tbordertop_box
-    }
-    print(item)
+    job = detail_html.xpath('//div[@class="cn"]/h1/@title')[0].strip()
+    salary = detail_html.xpath('//div[@class="cn"]//strong/text()')[0].strip()
+    company = detail_html.xpath('//p[@class="cname"]/a/text()')[0].strip()
+    area = detail_html.xpath('//p[@class="msg ltype"]/text()')[0].strip()
+    experience = detail_html.xpath('//p[@class="msg ltype"]/text()')[1].strip()
+    education = detail_html.xpath('//p[@class="msg ltype"]/text()')[2].strip()
+    companytype = detail_html.xpath('//p[@class="at"]/text()')[0].strip()
+    companyscale = detail_html.xpath('//p[@class="at"]/text()')[1].strip()
+    direction = detail_html.xpath('//p[@class="at"]/a/text()')[0].strip()
+    describe = detail_html.xpath('//div[@class="bmsg job_msg inbox"]//text()')
+    writer.writerow(
+        (url, job, salary, company, area, experience, education, companytype, companyscale, direction, describe)
+    )
 
 
-response = requests.get(url)
-response.encoding = 'gbk'
-html = etree.HTML(response.text)
-url_list = html.xpath('//div[@id="resultList"]//div[@class="el"]/p/span/a/@href')
-for url in url_list:
-    content_data(url)
+# 主调用函数
+# 抓取前三页信息
+for i in range(1, 3):
+    print('正在抓取第{}页信息'.format(i))
+    links = get_links(i)
+    for link in links:
+        try:
+            content_data(link)
+        except:
+            print("数据有缺失!")
+            continue
+
+# 关闭写入文件
+data.close()
