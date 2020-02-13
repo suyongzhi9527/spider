67ef1103a940a51ab4ea436a732440f378e74018
suyongzhi <1125699801@qq.com>
2020-02-13 09:58:50 +0800

    网易云音乐下载(去除特殊字符)

--- 网易云音乐/wangyi_spider.py
+++ 网易云音乐/wangyi_spider.py
@@ -1,9 +1,10 @@
 import requests
 import time
+import re
 from lxml import etree
 
 
-url = 'https://music.163.com/playlist?id=3226995486'
+url = 'https://music.163.com/playlist?id=3010352080'
 base_url = 'https://link.hhtjim.com/163/'
 
 headers = {
@@ -17,6 +18,7 @@ music_ids = dom.xpath('//a[contains(@href,"song?")]/@href')
 music_names = dom.xpath('//a[contains(@href,"song?")]/text()')
 
 for music_name, music_id in zip(music_names, music_ids):
+    music_name = re.sub(r'[\\:*?"<>|/]+', '', music_name)
     count_id = music_id.strip('/song?id=')
     if ('$' in count_id) == False:
         song_url = base_url + '%s' % count_id + '.mp3'
