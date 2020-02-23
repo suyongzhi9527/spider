6b9645d112caf162967052bec5a64ee1f0bbe4f3
suyongzhi <1125699801@qq.com>
2020-02-23 21:02:00 +0800

    拉钩网职位抓取

--- 拉钩网/urllib_Request_lagou.py
+++ 拉钩网/urllib_Request_lagou.py
@@ -1,4 +1,4 @@
-from urllib import request,parse
+from urllib import request, parse
 import requests
 import json
 
@@ -6,24 +6,24 @@ url = 'https://www.lagou.com/jobs/positionAjax.json?xl=%E5%A4%A7%E4%B8%93&px=def
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    'Referer':'https://www.lagou.com/jobs/list_python%E7%88%AC%E8%99%AB/p-city_0?&cl=false&fromSearch=true&labelWords=&suginput=',
-    'Origin':'https://www.lagou.com'
+    'Referer': 'https://www.lagou.com/jobs/list_python%E7%88%AC%E8%99%AB/p-city_0?&cl=false&fromSearch=true&labelWords=&suginput=',
+    'Origin': 'https://www.lagou.com'
 }
 
 data = {
-    'first':'true',
-    'pn':'1',
-    'kd':'python爬虫'
+    'first': 'true',
+    'pn': '1',
+    'kd': 'python爬虫'
 }
 
 urls = 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput='
 
 s = requests.Session()
 # 获取搜索页的cookies
-s.get(urls,headers = headers,timeout = 3)
+s.get(urls, headers=headers, timeout=3)
 
 cookie = s.cookies
 
-req = s.post(url,data=data,headers = headers,cookies = cookie,timeout = 5).text
+req = s.post(url, data=data, headers=headers, cookies=cookie, timeout=5).text
 dict_list = json.loads(req)
 print(dict_list['content']['positionResult']['result'])
