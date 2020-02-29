4cfade90edab2ee5fce48bf0c9453281c4f06025
suyongzhi <1125699801@qq.com>
2020-02-29 17:13:13 +0800

    腾讯疫情数据抓取保存到mysql数据库

--- 腾讯疫情数据/yiqing_data.py
+++ 腾讯疫情数据/yiqing_data.py
@@ -27,9 +27,89 @@ def close_conn(conn, cursor):
         conn.close()
 
 
+def get_tencent_history_data():
+    """
+    :return 返回历史数据
+    """
+    url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_other'
+    headers = {
+        'user-agent': 'Mozilla/5.0(Windows NT 10.0;Win64;x64) AppleWebKit/537.36(KHTML, likeGecko) Chrome/79.0.3945.117Safari/537.36'
+    }
+    r = requests.get(url, headers=headers)
+    res = json.loads(r.text)
+    data_all = json.loads(res['data'])
+    history = {}  # 保存历史数据
+    for i in data_all['chinaDayList']:
+        ds = "2020." + i["date"]
+        tup = time.strptime(ds, "%Y.%m.%d")
+        ds = time.strftime("%Y-%m-%d", tup)
+        confirm = i["confirm"]
+        suspect = i["suspect"]
+        heal = i["heal"]
+        dead = i["dead"]
+        history[ds] = {"confirm": confirm, "suspect": suspect, "heal": heal, "dead": dead}
+    for i in data_all["chinaDayAddList"]:
+        ds = "2020." + i["date"]
+        tup = time.strptime(ds, "%Y.%m.%d")
+        ds = time.strftime("%Y-%m-%d", tup)
+        confirm = i["confirm"]
+        suspect = i["suspect"]
+        heal = i["heal"]
+        dead = i["dead"]
+        history[ds].update({"confirm_add": confirm, "suspect_add": suspect, "heal_add": heal, "dead_add": dead})
+    return history
+
+
+def insert_history():
+    """
+    插入历史数据
+    """
+    cursor = None
+    conn = None
+    try:
+        dic = get_tencent_history_data()
+        print(f"{time.asctime()}开始插入数据")
+        conn, cursor = get_conn()
+        sql = "insert into history values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        for k, v in dic.items():
+            cursor.execute(sql, [k, v.get("confirm"), v.get("confirm_add"), v.get("suspect"),
+                                 v.get("suspect_add"),v.get("heal"), v.get("heal_add"),
+                                 v.get("dead"), v.get("dead_add")])
+        conn.commit()
+        print(f"{time.asctime()}插入历史数据完成!")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+
+def update_history():
+    """
+    更新历史数据
+    """
+    cursor = None
+    conn = None
+    try:
+        dic = get_tencent_history_data()
+        print(f"{time.asctime()}开始更新历史数据")
+        conn, cursor = get_conn()
+        sql = "insert into history values (%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
+        sql_query = "select confirm from history where ds = %s"
+        for k, v in dic.items():
+            if not cursor.execute(sql_query, k):
+                cursor.execute(sql, [k, v.get("conffirm"), v.get("confirm_add"), v.get("suspect"), v.get("suspect_add"),
+                                     v.get("heal"), v.get("heal_add"), v.get("dead"), v.get("dead_add")])
+        conn.commit()
+        print(f"{time.asctime()}历史数据更新完成!")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+
 def get_tencent_data():
     """
-    :return 返回历史数据和当日详细数据
+    :return 返回当日详细数据
     """
     url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
     headers = {
@@ -81,5 +161,7 @@ def update_detail():
 
 
 if __name__ == '__main__':
-    get_tencent_data()
-    update_detail()
+    # detail = get_tencent_data()
+    # update_detail()
+    # get_tencent_history_data()
+    insert_history()
