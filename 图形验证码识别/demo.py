5dc706bca0fb79b3cfd062358c713ab743f7ac7e
suyongzhi <1125699801@qq.com>
2020-03-16 22:33:32 +0800

    tesserocr图形验证码识别

--- 图形验证码识别/demo.py
+++ 图形验证码识别/demo.py
@@ -3,7 +3,16 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Tesseract-OCR\tesseract.exe'
 
-img = Image.open('1.jpg')
-
+img = Image.open('CheckCode.jfif')
+img = img.convert('L')
+thres = 140
+table = []
+for i in range(256):
+    if i < thres:
+        table.append(0)
+    else:
+        table.append(1)
+img = img.point(table, '1')
+img.show()
 img_str = pytesseract.image_to_string(img)
 print(img_str)
